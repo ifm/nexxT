b38da5870b803e59b92ad797bfe704c5ba68a6eb
Christoph Wiedemann <62332054+cwiede@users.noreply.github.com>
2020-10-22 10:58:56 +0200

    Update hdf5.py
    - fix hdf5 reader's suggested ports file dialog filters

--- nexxT/filters/hdf5.py
+++ nexxT/filters/hdf5.py
@@ -320,7 +320,7 @@ class Hdf5Reader(Filter):
     def onSuggestDynamicPorts(self):
         try:
             from PySide2.QtWidgets import QFileDialog
-            fn, ok = QFileDialog.getOpenFileName(caption="Choose template hdf5 file", filter="HDF5 files (*.h5 *.hdf5 *.hdf")
+            fn, ok = QFileDialog.getOpenFileName(caption="Choose template hdf5 file", filter="HDF5 files (*.h5 *.hdf5 *.hdf)")
             if ok:
                 f = h5py.File(fn, "r")
                 return [], [s for s in f["streams"]]
