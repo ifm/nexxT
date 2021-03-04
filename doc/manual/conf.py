658adaabe9113f4a3f5e71f92530007c328b7fa1
pfrydlewicz <30785616+pfrydlewicz@users.noreply.github.com>
2021-03-04 15:05:50 +0100

    Update conf.py

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -73,7 +73,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'manual']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'None'
 
 # -- Options for HTML output -------------------------------------------------
 
