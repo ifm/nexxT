7a96b0c1eb89486b10a9c6fe6162d3ecafb5eef5
Christoph Wiedemann <62332054+cwiede@users.noreply.github.com>
2023-07-21 10:45:44 +0200

    bump some package versions and adapt gui tests to not use --forked

--- workspace/pytest_nexxT.sh
+++ workspace/pytest_nexxT.sh
@@ -8,7 +8,7 @@ fi
 ADD_FLAGS=""
 
 # release variant
-                      "$PYTEST" -m     "gui" $ADD_FLAGS              --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html --forked ../nexxT/tests
+                      "$PYTEST" -m     "gui" $ADD_FLAGS              --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html -s ../nexxT/tests
                       "$PYTEST" -m "not gui" $ADD_FLAGS --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
 # other variants
 NEXXT_VARIANT=nonopt  "$PYTEST" -m "not gui" $ADD_FLAGS --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
