d23b40b9857f84e9331b9422611e17b88f7e5539
Christoph Wiedemann <62332054+cwiede@users.noreply.github.com>
2025-06-27 19:02:59 +0200

    bump pyside6 to 6.8.3

--- workspace/pytest_nexxT.sh
+++ workspace/pytest_nexxT.sh
@@ -5,12 +5,12 @@ else
   PYTEST="$1"
 fi
 
-ADD_FLAGS="--forked"
+ADD_FLAGS="--forked -n auto"
 
 # release variant
                       "$PYTEST" -m     "gui" $ADD_FLAGS              --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
-                      "$PYTEST" -m "not gui" $ADD_FLAGS --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
+                      "$PYTEST" -m "not gui" $ADD_FLAGS --dist=loadfile --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
 # other variants
-NEXXT_VARIANT=nonopt  "$PYTEST" -m "not gui" $ADD_FLAGS --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
-NEXXT_DISABLE_CIMPL=1 "$PYTEST" -m "not gui" $ADD_FLAGS --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
+NEXXT_VARIANT=nonopt  "$PYTEST" -m "not gui" $ADD_FLAGS --dist=loadfile --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
+NEXXT_DISABLE_CIMPL=1 "$PYTEST" -m "not gui" $ADD_FLAGS --dist=loadfile --cov-append --cov=../nexxT/core --cov=../nexxT/interface --cov=../nexxT/services --cov=../nexxT/filters --cov-report html ../nexxT/tests
 
